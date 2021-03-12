6afc8ec1f0aa650efc47b126aa5b1fb620b2b5ee
minebro190 <36795818+minebro190@users.noreply.github.com>
2021-03-12 12:52:34 +0800

    Add project files.

---
+++ Calculator With Ben/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_With_Ben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+        }
+    }
+}
