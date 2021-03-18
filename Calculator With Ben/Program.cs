1a90f0ba4b9eae3c8e3e6b57f29b3d8bad7bcd1a
minebro190 <36795818+minebro190@users.noreply.github.com>
2021-03-17 15:15:40 -1200

    The user is now able to choose which operation they would like choose, also adding a error message for invalid operators.

--- Calculator With Ben/Program.cs
+++ Calculator With Ben/Program.cs
@@ -10,13 +10,38 @@ namespace Calculator_With_Ben
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number:");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter a second number:");
-            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(num1 + num2);
 
+
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            String op1 = Console.ReadLine();
+
+            Console.Write("Enter a second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op1 == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op1 == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op1 == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op1 == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
             Console.ReadLine();
         }
     }
